a124616e0144ae8791d1b0225f38d1ee6fea78d1
Katanayka <katanaevdmitry45@gmail.com>
2023-04-26 00:33:01 +0500

    обменус 2

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -4,8 +4,12 @@
 </script>
 
 <header class="flex content-center justify-center sticky top-0 z-30 w-full h-14 px-4 pt-1 pb-2 mb-6 shadow-lg bg-orange-500">
-    <div class="w-2/3">
+    <div class="w-2/3 flex">
         <div class="font-sans font-semibold text-4xl text-slate-100">
+            <span class="shadow-md">🔁</span>
+            Обменус
+        </div>
+        <div class="font-sans font-semibold text-4xl text-slate-100 ml-auto ">
             <span class="shadow-md">🔁</span>
             Обменус
         </div>
